704aac76f484a4d7d19224095f88928578bede32
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-08-11 10:41:21 +0530

    Update Pattern-21.js

--- Pattern-21.js
+++ Pattern-21.js
@@ -2,7 +2,7 @@ const width = 10;
 
 for ( i = 1 ; i <= width ; i++){
   pattern = ' ';
-  for ( j = 1; j < i; j++){
+  for ( j = 1; j <= i; j++){
     pattern += (j + ' ');
   }
   console.log(pattern);
