bfd0a8f042438368da0ba5af9c091b7427f45429
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-06 09:48:45 +0530

    Update Pattern-28.js

--- JAVASCRIPT/Pattern-28.js
+++ JAVASCRIPT/Pattern-28.js
@@ -1,1 +1,9 @@
+const width = 10;
 
+for ( let i = 1 ; i <= (width + 1); i++){
+  pattern = '  '
+  for ( let j = 1; j <= ((width + 1) - i); j++ ){
+    pattern += (i + " ");
+  }
+  console.log(pattern );
+}
