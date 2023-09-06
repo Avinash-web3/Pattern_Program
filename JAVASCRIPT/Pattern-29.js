81c7b592c1f2c71b1884f7efa1dbb713992e4f4e
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-06 09:56:53 +0530

    Update Pattern-29.js

--- JAVASCRIPT/Pattern-29.js
+++ JAVASCRIPT/Pattern-29.js
@@ -3,7 +3,7 @@ const width = 10;
 for ( let i = 1 ; i <= (width + 1); i++){
   pattern = '  '
   for ( let j = 1; j <= ((width + 1) - i); j++ ){
-    pattern += (i + " ");
+    pattern += ((width + 1) - i + " ");
   }
   console.log(pattern );
 }
