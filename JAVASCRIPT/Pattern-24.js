8eb6d697cc89411455646d4fdd4aaea788500c81
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-08 10:10:53 +0530

    Update Pattern-24.js

--- JAVASCRIPT/Pattern-24.js
+++ JAVASCRIPT/Pattern-24.js
@@ -1,9 +1,10 @@
-const width = 10;
+let width = 10;
 
-for ( let i = 0 ; i < width; i++){
-  pattern = ' '
-  for ( let j = 1; j <= (width - i); j++ ){
-    pattern += '* ';
+for ( let i = 1; i <= width; i++){
+  let pattern = ''
+  for (let j = 1; j <= i; j++){
+    pattern += String.fromCharCode(64 + (width - (i - 1))) +(' ');
   }
-  console.log(pattern );
+  console.log(pattern);
 }
+
