64493da3a489de1ee1665d603ac8549dc90adb4a
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-08 09:52:13 +0530

    Update Pattern-22.js

--- JAVASCRIPT/Pattern-22.js
+++ JAVASCRIPT/Pattern-22.js
@@ -1,9 +1,10 @@
-const width = 10;
+let width = 10;
 
 for ( let i = 1; i <= width; i++){
-  pattern = ' '
-  for ( let j = 1; j <= i; j++ ){
-    pattern += (i+ ' ');
+  let pattern = ''
+  for (let j = 1; j <= i; j++){
+    pattern += String.fromCharCode(64 + j) + ( ' ');
   }
-  console.log(pattern );
+  console.log(pattern);
 }
+
