13fe99e0d4c2112d7b1933977c8865819001b425
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-08 10:27:06 +0530

    Update Pattern-27.js

--- JAVASCRIPT/Pattern-27.js
+++ JAVASCRIPT/Pattern-27.js
@@ -1,9 +1,9 @@
-const width = 10;
+let width = 10;
 
-for ( let i = width; i > 0; i--){
-  pattern = ' '
-  for ( let j = 1; j <= ((width + 1) -i); j++ ){
-    pattern += String.fromCharCode(64 + i) + " ";
+for ( let i = 1; i <= width; i++){
+  let pattern = ''
+  for (let j = 1; j <= i; j++){
+    pattern += i + (' ');
   }
-  console.log(pattern );
+  console.log(pattern);
 }
