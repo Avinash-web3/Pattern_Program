a70b25c592df5e2e80fd86f78a49c6b3dfde2535
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-08 10:05:34 +0530

    Update Pattern-23.js

--- JAVASCRIPT/Pattern-23.js
+++ JAVASCRIPT/Pattern-23.js
@@ -1,9 +1,10 @@
-const width = 10;
+let width = 10;
 
-for ( let i = width; i > 0; i--){
-  pattern = ' '
-  for ( let j = 1; j <= ((width + 1) -i); j++ ){
-    pattern += (i+ ' ');
+for ( let i = 1; i <= width; i++){
+  let pattern = ''
+  for (let j = 1; j <= i; j++){
+    pattern += (width - (i - 1)) +(' ');
   }
-  console.log(pattern );
+  console.log(pattern);
 }
+
