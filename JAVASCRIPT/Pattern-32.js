a17923f792099f5069e458f921b753833720ac5a
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-09 10:09:13 +0530

    Create Pattern-32.js

---
+++ JAVASCRIPT/Pattern-32.js
@@ -0,0 +1,10 @@
+let width = 10;
+
+for ( let i = 1; i <= width; i++){
+  let pattern = '';
+  for ( let j = 1; j <= (width - i + 1); j++){
+    pattern += i + ' ';
+  }
+  console.log(pattern);
+}
+
