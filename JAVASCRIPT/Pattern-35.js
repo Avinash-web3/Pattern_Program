00a74cd224c78447981e5ea8ac4349c131e49214
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-09 10:39:36 +0530

    Update Pattern-35.js

--- JAVASCRIPT/Pattern-35.js
+++ JAVASCRIPT/Pattern-35.js
@@ -3,8 +3,9 @@ let width = 10;
 for ( let i = 1; i <= width; i++){
   let pattern = '';
   for ( let j = 1; j <= (width - i + 1); j++){
-    pattern +=  String.fromCharCode (64 + (width - j - i + 2))  + ' ';
+    pattern +=  (width - i  + 1)  + ' ';
   }
   console.log(pattern);
 }
+
 
