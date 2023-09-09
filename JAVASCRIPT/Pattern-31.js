8be69550b322395e5a47f579ad504ab205ac4924
Avinash-web3 <130075425+Avinash-web3@users.noreply.github.com>
2023-09-09 10:04:58 +0530

    Update Pattern-31.js

--- JAVASCRIPT/Pattern-31.js
+++ JAVASCRIPT/Pattern-31.js
@@ -1,9 +1,9 @@
-const width = 10;
+let width = 10;
 
-for ( let i = 1 ; i <= (width + 1); i++){
-  pattern = '  '
-  for ( let j = 1; j <= ((width + 1) - i); j++ ){
-    pattern += (String.fromCharCode( 64 + i) + " ");
+for ( let i = 1; i <= width; i++){
+  let pattern = '';
+  for ( let j = 1; j <= (width - i + 1); j++){
+    pattern += String.fromCharCode(64 + j) + ' ';
   }
-  console.log(pattern );
+  console.log(pattern);
 }
